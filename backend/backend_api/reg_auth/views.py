53f5c26955c6ac3095318208dd99d98a89f2ce2c
Rory Maclean <roryobm@outlook.com>
2024-03-06 16:31:06 +1100

    user authentication/registration boilerplate

---
+++ backend/backend_api/reg_auth/views.py
@@ -0,0 +1,26 @@
+
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth import login
+from backend_api.reg_auth.forms import SignUpForm
+
+def signup_view(request):
+    if request.method == 'POST':
+        form = SignUpForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('login')
+    else:
+        form = SignUpForm()
+    return render(request, 'signup.html', {'form': form})
+
+def login_view(request):
+    if request.method == 'POST':
+        form = AuthenticationForm(data=request.POST)
+        if form.is_valid():
+            user = form.get_user()
+            login(request, user)
+            return redirect('home')
+    else:
+        form = AuthenticationForm()
+    return render(request, 'login.html', {'form': form})
--- backend/backend_api/urls.py
+++ backend/backend_api/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from . import views
+from backend_api.reg_auth import views
 
 urlpatterns = [
-    path('hello-world/', views.hello_world, name='hello_world'),
+    path('signup/', views.signup_view, name='signup'),
+    path('login/', views.login_view, name='login'),
 ]

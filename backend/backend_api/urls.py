0869f5e7e1870299529402bbfae06a3f61e674a3
Rory Maclean <roryobm@outlook.com>
2024-03-09 15:16:25 +1100

    makemigration updates

--- backend/backend_api/urls.py
+++ backend/backend_api/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from backend_api.reg_auth import views
 
 urlpatterns = [
-    path('signup/', views.signup, name='signup'),
-    path('login/', views.login, name='login'),
+    path('signup/', views.user_signup, name='signup'),
+    path('login/', views.user_login, name='login'),
     path('delete_account/', views.delete_account, name='delete_account'),
 ]
--- backend/backend_project/settings.py
+++ backend/backend_project/settings.py
@@ -43,9 +43,9 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
-    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -53,7 +53,8 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-CORS_ORIGIN_ALLOW_ALL = True
+CORS_ALLOW_ALL_ORIGINS = True
+CORS_ALLOW_CREDENTIALS = False
 
 ROOT_URLCONF = 'backend_project.urls'
 

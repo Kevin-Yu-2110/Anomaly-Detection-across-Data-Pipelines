3d319a64383598ce08e3f1a5a9943b2795f2094f
Jason Guo <z5114601@ad.unsw.edu.au>
2024-03-19 03:15:42 +1100

    implement delete_account route on frontend

--- frontend/src/pages/HomePage/components/user_profile.js
+++ frontend/src/pages/HomePage/components/user_profile.js
@@ -2,20 +2,59 @@ import React, { useState } from "react";
 import { Button, Form, Modal } from "react-bootstrap";
 import { BsPersonCircle } from "react-icons/bs";
 import { useUser } from "../../../UserContext";
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
+import { toast } from "react-toastify";
 
 const UserProfile = () => {
-  const [show, setShow] = useState(false);
-  const {username} = useUser();
+  const navigate = useNavigate();
+  const [showProfile, setShowProfile] = useState(false);
+  const [showConfirm, setShowConfirm] = useState(false);
+  const {user_logout, username, token} = useUser();
 
-  const handleClose = () => setShow(false);
-  const handleShow = () => setShow(true);
+  const handleCloseProfile = () => setShowProfile(false);
+  const handleShowProfile = () => setShowProfile(true);
+
+  const handleCloseConfirm = () => setShowConfirm(false);
+  const handleShowConfirm = () => setShowConfirm(true);
+
+  const deleteFailed = () => toast.error("Delete account failed");
+
+  const confirmDelete = () => {
+    handleCloseProfile();
+    handleShowConfirm();
+  }
+
+  const deleteAccount = async (e) => {
+    e.preventDefault();
+    try {
+      const response = await axios.post("http://127.0.0.1:8000/api/delete_account/",
+        {
+          username
+        },
+        {
+          headers: {
+            Authorization: `Bearer ${token}`
+          }
+        }
+      );
+      if (response.data.success) {
+        user_logout();
+        navigate("/");
+      } else {
+        deleteFailed();
+      }
+    } catch (error) {
+      console.error("Delete account failed: Server-Side Error", error);
+    }
+  }
 
   return (
     <>
-      <BsPersonCircle className="icon" onClick={handleShow}></BsPersonCircle>
+      <BsPersonCircle className="icon" onClick={handleShowProfile}></BsPersonCircle>
 
       {/** user profile modal */}
-      <Modal show={show} onHide={handleClose}>
+      <Modal show={showProfile} onHide={handleCloseProfile}>
         <Modal.Header closeButton>
           <Modal.Title>User Profile</Modal.Title>
         </Modal.Header>
@@ -42,14 +81,27 @@ const UserProfile = () => {
           </Form>
         </Modal.Body>
         <Modal.Footer>
-          <Button variant="danger"> {/** functionality needed */}
+          <Button variant="danger" onClick={confirmDelete}> 
             Delete Account
           </Button>
           <Button variant="secondary"> {/** functionality needed */}
             Edit
           </Button>
-          <Button variant="primary" onClick={handleClose}>
+          <Button variant="primary" onClick={handleCloseProfile}>
             Save Changes
+          </Button>
+        </Modal.Footer>
+      </Modal>
+
+      {/** confirm delete account modal */}
+      <Modal show={showConfirm} onHide={handleCloseConfirm}>
+        <Modal.Header closeButton></Modal.Header>
+        <Modal.Body>
+          <div>Are you sure you want to delete your account?</div>
+        </Modal.Body>
+        <Modal.Footer>
+          <Button variant="danger" onClick={deleteAccount}>
+            Delete Account
           </Button>
         </Modal.Footer>
       </Modal>

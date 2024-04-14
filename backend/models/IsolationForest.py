bccdf4ba0672309d33cc178b2b5a5eba4e4583bc
unknown <z5363805@ad.unsw.edu.au>
2024-04-15 01:54:45 +1000

    added folder for encoders for unique individuals

--- backend/models/IsolationForest.py
+++ backend/models/IsolationForest.py
@@ -7,8 +7,8 @@ import pandas as pd
 from sklearn.metrics import confusion_matrix
 from sklearn.ensemble import IsolationForest
 from sklearn.preprocessing import LabelEncoder
-from clean_up import clean_up
-from abstract_model import abstract_model
+from models.clean_up import clean_up
+from models.abstract_model import abstract_model
 
 import mlflow
 from mlflow import MlflowClient
@@ -17,13 +17,13 @@ from mlflow import MlflowClient
 class isolationForestModel(abstract_model):
     def __init__(self, owner):
         try:
-            encoder_path = os.path.join(os.path.dirname(__file__), 'Encoder.pickle')
+            encoder_path = os.path.join(os.path.dirname(__file__), 'encoders/' + self.model_name + '-encoder.pickle')
             with open(encoder_path, 'rb') as encoder:
                 encoder = pickle.load(encoder)
             model_name = 'default-IF'
         except Exception as e:
             model_name, encoder = train_model()
-            encoder_path = os.path.join(os.path.dirname(__file__), 'Encoder.pickle')
+            encoder_path = os.path.join(os.path.dirname(__file__), 'encoders/default_encoder.pickle')
             with open(encoder_path, 'wb') as handle:
                 pickle.dump(encoder, handle)
             print(encoder)
@@ -52,18 +52,18 @@ class isolationForestModel(abstract_model):
         
     def retrain(self, X):
         model_name, encoder = train_model(pd.DataFrame(X, columns = ['trans_date_trans_time', 'cc_num', 'merchant', 'category', 'amt', 'city', 'job', 'dob', 'class']), self.owner)
-        encoder_path = os.path.join(os.path.dirname(__file__), 'Encoder.pickle')
+        encoder_path = os.path.join(os.path.dirname(__file__), 'encoders/' + self.model_name + '-encoder.pickle')
         with open(encoder_path, 'wb') as handle:
             pickle.dump(encoder, handle)
         self.encoder = encoder
 
 def train_model(data=pd.DataFrame(), owner=None):
     # enable autologging
-    mlflow.sklearn.autolog()
+    #mlflow.sklearn.autolog()
 
     remote_server_uri = "http://127.0.0.1:5000"
     mlflow.set_tracking_uri(remote_server_uri)
-    mlflow.set_experiment("isolationforest")
+    #mlflow.set_experiment("isolationforest")
 
     train_path = os.path.join(os.path.dirname(__file__), 'fraudTrain.csv')
     train_data = pd.read_csv(train_path)
@@ -74,25 +74,25 @@ def train_model(data=pd.DataFrame(), owner=None):
     y_train = train_data.iloc[:, -1:]
 
     random_state = np.random.RandomState(42)
-    test_data = pd.read_csv('backend/models/fraudTest.csv')
-    X_test, enc = clean_up(test_data.iloc[:, :-1], enc)
-    y_test = test_data.iloc[:, -1:]
+    #test_data = pd.read_csv('backend/models/fraudTest.csv')
+    #X_test, enc = clean_up(test_data.iloc[:, :-1], enc)
+    #y_test = test_data.iloc[:, -1:]
 
     model = IsolationForest(n_estimators=100,max_samples='auto',contamination=float(0.2),random_state=random_state)
     with mlflow.start_run() as run:
         model.fit(X_train, y_train)
 
-        y_actual = y_test.to_numpy().transpose()[0]
-        y_predict = model.predict(X_test)
-        y_predict[y_predict == 1] = 0
-        y_predict[y_predict == -1] = 1
-        count = (y_actual == y_predict).sum()
-        mlflow.log_metric("accuracy", count/len(y_actual))
-        false_positives = sum(1 if (y_actual[i] == 0 and y_predict[i] == 1) else 0 for i in range(len(y_actual)))
-        mlflow.log_metric("false positive rate", false_positives/len(y_actual))
-        false_negatives = sum(1 if (y_actual[i] == 1 and y_predict[i] == 0) else 0 for i in range(len(y_actual)))
-        mlflow.log_metric("false negative rate", false_negatives/len(y_actual))
-        print(confusion_matrix(y_actual, y_predict))
+        #y_actual = y_test.to_numpy().transpose()[0]
+        #y_predict = model.predict(X_test)
+        #y_predict[y_predict == 1] = 0
+        #y_predict[y_predict == -1] = 1
+        #count = (y_actual == y_predict).sum()
+        #mlflow.log_metric("accuracy", count/len(y_actual))
+        #false_positives = sum(1 if (y_actual[i] == 0 and y_predict[i] == 1) else 0 for i in range(len(y_actual)))
+        #mlflow.log_metric("false positive rate", false_positives/len(y_actual))
+        #false_negatives = sum(1 if (y_actual[i] == 1 and y_predict[i] == 0) else 0 for i in range(len(y_actual)))
+        #mlflow.log_metric("false negative rate", false_negatives/len(y_actual))
+        #print(confusion_matrix(y_actual, y_predict))
         model_name = owner + '-IF' if owner else 'default-IF' 
         
         mlflow.sklearn.log_model(
